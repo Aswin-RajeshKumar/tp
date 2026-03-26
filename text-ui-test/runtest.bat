d6b4ad98c440ef401da489ff8fb575483fff0aea
Abigail Tong <abigail@Abigails-MacBook-Air.local>
2026-03-26 18:35:04 +0800

    Update Windows runtest file

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,12 +6,14 @@ cd ..
 call gradlew clean shadowJar
 
 cd build\libs
-set jarloc=duke.jar
+for /f "tokens=*" %%a in (
+    'dir /b *.jar'
+) do (
+    set jarloc=%%a
+)
 
-java -cp %jarloc% app.JobPilot < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
+java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
 
 cd ..\..\text-ui-test
 
 FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
-
-pause
